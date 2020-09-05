b3848d76f24cf19c85d9404474f644d4d8a6d20f
Ravi Teja Kumar Isetty <iravitejakumar@gmail.com>
2020-09-05 11:28:49 +0530

    Added blogs list cards

--- src/pages/blogs-list.js
+++ src/pages/blogs-list.js
@@ -5,23 +5,26 @@ import SEO from "../components/seo"
 import BlogBox from '../components/blog-box';
 
 const blogs = [
-    {name: "React andn Redux best Practices", img: 'react.jpeg', tags: [{name: 'React'}, {name: 'Javascript'}]},
-    {name: "React andn Redux best Practices", img: 'react.jpeg', tags: [{name: 'React'}, {name: 'Javascript'}]}, 
-    {name: "React andn Redux best Practices", img: 'react.jpeg', tags: [{name: 'React'}, {name: 'Javascript'}]}, 
-    {name: "React andn Redux best Practices", img: 'react.jpeg', tags: [{name: 'React'}, {name: 'Javascript'}]},
-    {name: "React andn Redux best Practices", img: 'react.jpeg', tags: [{name: 'React'}, {name: 'Javascript'}]},
-    {name: "React andn Redux best Practices", img: 'react.jpeg', tags: [{name: 'React'}, {name: 'Javascript'}]}, 
-    {name: "React andn Redux best Practices", img: 'react.jpeg', tags: [{name: 'React'}, {name: 'Javascript'}]}, 
-    {name: "React andn Redux best Practices", img: 'react.jpeg', tags: [{name: 'React'}, {name: 'Javascript'}]},
+  { name: "React andn Redux best Practices", img: 'react.jpeg', tags: [{ name: 'React' }, { name: 'Javascript' }] },
+  { name: "React andn Redux best Practices", img: 'react.jpeg', tags: [{ name: 'React' }, { name: 'Javascript' }] },
+  { name: "React andn Redux best Practices", img: 'gatsby-astronaut.png', tags: [{ name: 'React' }, { name: 'Javascript' }] },
+  { name: "React andn Redux best Practices", img: 'react.jpeg', tags: [{ name: 'React' }, { name: 'Javascript' }] },
+  { name: "React andn Redux best Practices", img: 'react.jpeg', tags: [{ name: 'React' }, { name: 'Javascript' }] },
+  { name: "React andn Redux best Practices", img: 'gatsby-astronaut.png', tags: [{ name: 'React' }, { name: 'Javascript' }] },
+  { name: "React andn Redux best Practices", img: 'react.jpeg', tags: [{ name: 'React' }, { name: 'Javascript' }] },
+  { name: "React andn Redux best Practices", img: 'react.jpeg', tags: [{ name: 'React' }, { name: 'Javascript' }] },
 ]
 const BlogsList = () => (
   <Layout>
     <SEO title="Page two" />
-    <div className="blogs-list">
-     {
-        blogs.map((blog, index) => <BlogBox key={index} blog={blog}/>) 
-     }
-     </div>
+    <div className="blog-container">
+      <div className="blog-filters">Filters</div>
+      <div className="blogs-list">
+        {
+          blogs.map((blog, index) => <BlogBox key={index} blog={blog} />)
+        }
+      </div>
+    </div>
   </Layout>
 )
 
